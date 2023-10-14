058f1ada3fc46a820e0211be49b083d1a01819de
nza34 <nicholas_zhang_2@sfu.ca>
2023-10-14 12:05:06 -0700

    thread_function.c changes

--- thread_functions.c
+++ thread_functions.c
@@ -1,28 +1,22 @@
 #include "thread_functions.h"
 
-void* send_thread(void* arg) {
-    // Potential Loop to keep the thread alive to send multiple messages
-    while (1) {
-        // Code to fetch and/or format data to send
-        // ...
-
-        // Call your send socket function
-        socket_send( );
-
-        // Optional: Implement delay or conditional wait here
-        // ...
+void* send_thread_func(void* arg) {
+    const char* ip_address = "127.0.0.1"; // Example IP
+    int port = 22110; // Example port
+    const char* message = "Hello, Server!";
+    
+    while(1) { 
+        socket_send(ip_address, port, message);
+        sleep(1); // Prevents spamming messages non-stop
     }
+    
     return NULL;
 }
 
-void* receive_thread(void* arg) {
-    // Potential Loop to keep the thread alive for continuous listening
-
-    // Call your receive socket function
-    socket_receive( /* Parameters as per your function */ );
-
-    // Optional: Implement a mechanism to process or display the received data
-    // ...
+void* receive_thread_func(void* arg) {
+    int port = 22110; // Example port
+    
+    socket_receive(port);
     
     return NULL;
 }
